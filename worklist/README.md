a3021d7a7334029ac1bb95a2e0d4ec7a6a7d0f91
Albert ten Napel <aptennap@gmail.com>
2019-07-17 14:19:26 +0200

    kind checking

--- worklist/README.md
+++ worklist/README.md
@@ -7,3 +7,7 @@ I also use meta type variables for the synthesis judgments (for the same reason)
 Questions:
 - How/when to do generalization?
 - Can the system be easily extended to do kind inference (on the type annotations)
+
+TODO:
+- add kmeta, turn wfType in to judgment
+- kind inference
--- worklist/worklist.js
+++ worklist/worklist.js
@@ -1,14 +1,33 @@
+// kinds
+const KVar = name => ({ tag: 'KVar', name });
+const KFun = (left, right) => ({ tag: 'KFun', left, right });
+
+const kType = KVar('Type');
+
+const showKind = ki => {
+  if (ki.tag === 'KVar') return ki.name;
+  if (ki.tag === 'KFun') return `(${showKind(ki.left)} -> ${showKind(ki.right)})`;
+};
+
+const eqKind = (a, b) => {
+  if (a === b) return true;
+  if (a.tag === 'KVar') return b.tag === 'KVar' && a.name === b.name;
+  if (a.tag === 'KFun')
+    return b.tag === 'KFun' && eqKind(a.left, b.left) && eqKind(a.right, b.right);
+  return false;
+};
+
 // types
 let tmetaId = 0;
 const freshTMetaId = () => tmetaId++;
 const resetTMetaId = () => { tmetaId = 0 };
-const TMeta = id => ({ tag: 'TMeta', id, type: null });
-const freshTMeta = () => TMeta(freshTMetaId());
+const TMeta = (id, kind) => ({ tag: 'TMeta', id, kind, type: null });
+const freshTMeta = kind => TMeta(freshTMetaId(), kind);
 
 const TVar = name => ({ tag: 'TVar', name });
 const TApp = (left, right) => ({ tag: 'TApp', left, right });
-const TForall = (name, type) => ({ tag: 'TForall', name, type });
-const tforall = (ns, t) => ns.reduceRight((t, x) => TForall(x, t), t);
+const TForall = (name, kind, type) => ({ tag: 'TForall', name, kind, type });
+const tforall = (ns, t) => ns.reduceRight((t, [x, k]) => TForall(x, k, t), t);
 
 const tFun = TVar('->');
 const TFun = (left, right) => TApp(TApp(tFun, left), right);
@@ -17,11 +36,11 @@ const tfunL = ty => ty.left.right;
 const tfunR = ty => ty.right;
 
 const showType = type => {
-  if (type.tag === 'TMeta') return `?${type.id}${type.type ? `{${showType(type.type)}}` : ''}`;
+  if (type.tag === 'TMeta') return `?${type.id}(${showKind(type.kind)})${type.type ? `{${showType(type.type)}}` : ''}`;
   if (type.tag === 'TVar') return type.name;
   if (isTFun(type)) return `(${showType(tfunL(type))} -> ${showType(tfunR(type))})`;
   if (type.tag === 'TApp') return `(${showType(type.left)} ${showType(type.right)})`;
-  if (type.tag === 'TForall') return `(forall ${type.name}. ${showType(type.type)})`;
+  if (type.tag === 'TForall') return `(forall(${type.name} : ${showKind(type.kind)}). ${showType(type.type)})`;
 };
 
 const pruneTop = type => {
@@ -44,7 +63,7 @@ const prune = type => {
   }
   if (type.tag === 'TForall') {
     const b = prune(type.type);
-    return b === type.type ? type : TForall(type.name, b);
+    return b === type.type ? type : TForall(type.name, type.kind, b);
   }
   return type;
 };
@@ -59,7 +78,7 @@ const substTVar = (x, s, t) => {
   if (t.tag === 'TForall') {
     if (t.name === x) return t;
     const b = substTVar(x, s, t.type);
-    return b === t.type ? t : TForall(t.name, b);
+    return b === t.type ? t : TForall(t.name, t.kind, b);
   }
   return t;
 };
@@ -88,7 +107,7 @@ const tmetas = (t, a = []) => {
 // terms
 const Var = name => ({ tag: 'Var', name });
 const Abs = (name, body) => ({ tag: 'Abs', name, body });
-const AbsT = (name, body, type) => ({ tag: 'AbsT', name, body, type });
+const AbsT = (name, kind, body, type) => ({ tag: 'AbsT', name, kind, body, type });
 const App = (left, right) => ({ tag: 'App', left, right });
 const AppT = (left, right) => ({ tag: 'AppT', left, right });
 const Ann = (term, type) => ({ tag: 'Ann', term, type });
@@ -99,7 +118,7 @@ const showTerm = term => {
   if (term.tag === 'Var') return term.name;
   if (term.tag === 'Abs') return `(\\${term.name} -> ${showTerm(term.body)})`;
   if (term.tag === 'AbsT')
-    return `(/\\${term.name} -> (${showTerm(term.body)} : ${showType(term.type)}))`;
+    return `(/\\(${term.name} : ${showKind(term.kind)}) -> (${showTerm(term.body)} : ${showType(term.type)}))`;
   if (term.tag === 'App') return `(${showTerm(term.left)} ${showTerm(term.right)})`;
   if (term.tag === 'AppT') return `(${showTerm(term.left)} @${showType(term.right)})`;
   if (term.tag === 'Ann') return `(${showTerm(term.term)} : ${showType(term.type)})`;
@@ -127,12 +146,15 @@ const showJudgment = judgment => {
     return `${showType(judgment.type1)} @${showType(judgment.type2)} =>>${showType(judgment.result)} (${showJudgment(judgment.cont)})`;
 };
 
-// elems (includes TVar, TMeta and judgments)
+// elems (includes KVar, TMeta and judgments)
+const CTVar = (name, kind) => ({ tag: 'CTVar', name, kind });
 const CVar = (name, type) => ({ tag: 'CVar', name, type });
 
 const showElem = elem => {
+  if (elem.tag === 'CTVar') return `${elem.name} : ${showKind(elem.kind)}`;
   if (elem.tag === 'CVar') return `${elem.name} : ${showType(elem.type)}`;
-  if (elem.tag === 'TVar' || elem.tag === 'TMeta') return showType(elem);
+  if (elem.tag === 'KVar') return showKind(elem);
+  if (elem.tag === 'TMeta') return showType(elem);
   return showJudgment(elem);
 };
 
@@ -146,47 +168,80 @@ const findVar = (wl, x) => {
   }
   return null;
 };
+const indexKVar = (wl, x) => {
+  for (let i = wl.length - 1; i >= 0; i--) {
+    const c = wl[i];
+    if (c.tag === 'KVar' && c.name === x) return i;
+  }
+  return -1;
+};
 const indexTVar = (wl, x) => {
   for (let i = wl.length - 1; i >= 0; i--) {
     const c = wl[i];
-    if (c.tag === 'TVar' && c.name === x) return i;
+    if (c.tag === 'CTVar' && c.name === x) return i;
   }
   return -1;
 };
 const indexTMeta = (wl, m) => wl.indexOf(m);
 
+const kindTVar = (wl, x) => {
+  const i = indexTVar(wl, x);
+  if (i < 0) return null;
+  return wl[i].kind;
+};
+
 const remove = (wl, i) => wl.splice(i, 1);
 const replace2 = (wl, i, a, b) => wl.splice(i, 1, a, b);
 
-const initialContext = () => [tFun];
+const initialContext = () => [
+  kType,
+  CTVar(tFun.name, KFun(kType, KFun(kType, kType))),
+];
 
 // errors
 const terr = msg => { throw new TypeError(msg) };
 
 // wellformedness
-const wfTypeR = (wl, t) => {
-  if (t.tag === 'TVar') return indexTVar(wl, t.name) >= 0;
-  if (t.tag === 'TMeta') return indexTMeta(wl, t) >= 0;
-  if (t.tag === 'TApp') return wfTypeR(wl, t.left) && wfTypeR(wl, t.right);
-  if (t.tag === 'TForall') {
-    wl.push(TVar(t.name));
-    return wfTypeR(wl, t.type);
-  }
+const wfKind = (wl, k) => {
+  if (k.tag === 'KVar') return indexKVar(wl, k.name) >= 0;
+  if (k.tag === 'KFun') return wfKind(wl, k.left) && wfKind(wl, k.right);
 };
+
 const wfType = (wl, t) => {
-  const l = wl.length;
-  const b = wfTypeR(wl, t);
-  wl.splice(l, wl.length);
-  return b;
+  if (t.tag === 'TVar') {
+    const i = indexTVar(wl, t.name);
+    return i >= 0 ? wl[i].kind : null;
+  }
+  if (t.tag === 'TMeta')
+    return indexTMeta(wl, t) >= 0 && wfKind(wl, t.kind) ? t.kind : null;
+  if (t.tag === 'TApp') {
+    const kl = wfType(wl, t.left);
+    if (!kl) return null;
+    if (kl.tag !== 'KFun') return null;
+    const kr = wfType(wl, t.right);
+    if (!kr) return null;
+    if (!eqKind(kr, kl.left)) return null;
+    return kl.right;
+  }
+  if (t.tag === 'TForall') {
+    if (!wfKind(wl, t.kind)) return null;
+    const l = wl.length;
+    wl.push(CTVar(t.name, t.kind));
+    const k = wfType(wl, t.type);
+    wl.splice(l, wl.length);
+    if (!k || !eqKind(k, kType)) return null;
+    return k;
+  }
 };
 
 // algorithm
 const step = wl => {
   const top = wl.pop();
   
-  if (top.tag === 'TVar') return;
-  if (top.tag === 'TMeta') return;
+  if (top.tag === 'KVar') return;
+  if (top.tag === 'CTVar') return;
   if (top.tag === 'CVar') return;
+  if (top.tag === 'TMeta') return;
 
   if (top.tag === 'JDone') return;
 
@@ -210,9 +265,9 @@ const step = wl => {
         JSubtype(left.left, right.left),
       );
     if (right.tag === 'TForall')
-      return wl.push(TVar(right.name), JSubtype(left, right.type));
+      return wl.push(CTVar(right.name, right.kind), JSubtype(left, right.type));
     if (left.tag === 'TForall') {
-      const m = freshTMeta();
+      const m = freshTMeta(left.kind);
       return wl.push(m, JSubtype(openTForall(left, m), right));
     }
     if (left.tag === 'TMeta' && right.tag === 'TApp') {
@@ -220,9 +275,12 @@ const step = wl => {
       if (i < 0) return terr(`undefined tmeta ${showType(left)}`);
       if (occursTMeta(left, right))
         return terr(`occurs check failed ${showJudgment(top)}`);
-      const a = freshTMeta();
-      const b = freshTMeta();
-      const ty = (isTFun(right) ? TFun : TApp)(a, b);
+      const k = wfType(wl, right.left);
+      if (!eqKind(left.kind, k.right)) return terr(`kind mismatch: ${showJudgment(top)}`);
+      const isFun = isTFun(right);
+      const a = freshTMeta(isFun ? kType : k);
+      const b = freshTMeta(isFun ? kType : k.left);
+      const ty = (isFun ? TFun : TApp)(a, b);
       left.type = ty;
       replace2(wl, i, a, b);
       return wl.push(JSubtype(ty, right));
@@ -232,9 +290,12 @@ const step = wl => {
       if (i < 0) return terr(`undefined tmeta ${showType(right)}`);
       if (occursTMeta(right, left))
         return terr(`occurs check failed ${showJudgment(top)}`);
-      const a = freshTMeta();
-      const b = freshTMeta();
-      const ty = (isTFun(left) ? TFun : TApp)(a, b);
+      const k = wfType(wl, left.left);
+      if (!eqKind(right.kind, k.right)) return terr(`kind mismatch: ${showJudgment(top)}`);
+      const isFun = isTFun(left);
+      const a = freshTMeta(isFun ? kType : k);
+      const b = freshTMeta(isFun ? kType : k.left);
+      const ty = (isFun ? TFun : TApp)(a, b);
       right.type = ty;
       replace2(wl, i, a, b);
       return wl.push(JSubtype(left, ty));
@@ -244,6 +305,7 @@ const step = wl => {
       if (i < 0) return terr(`undefined tmeta ${showType(left)}`);
       const j = indexTMeta(wl, right);
       if (j < 0) return terr(`undefined tmeta ${showType(right)}`);
+      if (!eqKind(left.kind, right.kind)) return terr(`kind mismatch: ${showJudgment(top)}`);
       if (i < j) {
         remove(wl, i);
         left.type = right;
@@ -259,6 +321,7 @@ const step = wl => {
       const j = indexTMeta(wl, right);
       if (j < 0) return terr(`undefined tmeta ${showType(right)}`);
       if (i > j) return terr(`tvar out of scope ${showJudgment(top)} in ${showWorklist(wl)}`);
+      if (!eqKind(wl[i].kind, right.kind)) return terr(`kind mismatch: ${showJudgment(top)}`);
       remove(wl, j);
       right.type = left;
       return;
@@ -269,6 +332,7 @@ const step = wl => {
       const j = indexTVar(wl, right.name);
       if (j < 0) return terr(`undefined tvar ${showType(right)}`);
       if (j > i) return terr(`tvar out of scope ${showJudgment(top)} in ${showWorklist(wl)}`);
+      if (!eqKind(wl[j].kind, left.kind)) return terr(`kind mismatch: ${showJudgment(top)}`);
       remove(wl, i);
       left.type = right;
       return;
@@ -280,22 +344,22 @@ const step = wl => {
     const { term, type: type_ } = top;
     const type = pruneTop(type_);
     if (type.tag === 'TForall')
-      return wl.push(TVar(type.name), JCheck(term, type.type));
+      return wl.push(CTVar(type.name, type.kind), JCheck(term, type.type));
     if (term.tag === 'Abs') {
       if (isTFun(type))
         return wl.push(CVar(term.name, tfunL(type)), JCheck(term.body, tfunR(type)));
       if (type.tag === 'TMeta') {
         const i = indexTMeta(wl, type);
         if (i < 0) return terr(`undefined tmeta ${showType(type)}`);
-        const a = freshTMeta();
-        const b = freshTMeta();
+        const a = freshTMeta(kType);
+        const b = freshTMeta(kType);
         const ty = TFun(a, b);
         type.type = ty;
         replace2(wl, i, a, b);
         return wl.push(CVar(term.name, a), JCheck(term.body, b));
       }
     }
-    const result = freshTMeta();
+    const result = freshTMeta(kType);
     return wl.push(JSynth(term, result, JSubtype(result, type)));
   }
 
@@ -308,27 +372,28 @@ const step = wl => {
       return wl.push(cont);
     }
     if (term.tag === 'Ann') {
-      if (!wfType(wl, term.type))
-        return terr(`type not wellformed: ${showJudgment(top)}`);
+      const k = wfType(wl, term.type);
+      if (!k) return terr(`type not wellformed: ${showJudgment(top)}`);
+      if (!eqKind(k, kType)) return terr(`type annotation must be of kind ${showKind(kType)}: ${showJudgment(top)}`);
       result.type = term.type;
       return wl.push(cont, JCheck(term.term, term.type));
     }
     if (term.tag === 'Abs') {
-      const a = freshTMeta();
-      const b = freshTMeta();
+      const a = freshTMeta(kType);
+      const b = freshTMeta(kType);
       result.type = TFun(a, b);
       return wl.push(a, b, cont, CVar(term.name, a), JCheck(term.body, b));
     }
     if (term.tag === 'AbsT') {
-      result.type = TForall(term.name, term.type);
-      return wl.push(cont, TVar(term.name), JCheck(term.body, term.type));
+      result.type = TForall(term.name, term.kind, term.type);
+      return wl.push(cont, CTVar(term.name, term.kind), JCheck(term.body, term.type));
     }
     if (term.tag === 'App') {
-      const result2 = freshTMeta();
+      const result2 = freshTMeta(kType);
       return wl.push(JSynth(term.left, result2, JSynthapp(result2, term.right, result, cont)));
     }
     if (term.tag === 'AppT') {
-      const result2 = freshTMeta();
+      const result2 = freshTMeta(kType);
       return wl.push(JSynth(term.left, result2, JSynthappT(result2, term.right, result, cont)));
     }
   }
@@ -337,7 +402,7 @@ const step = wl => {
     const { type: type_, term, result, cont } = top;
     const type = pruneTop(type_);
     if (type.tag === 'TForall') {
-      const a = freshTMeta();
+      const a = freshTMeta(type.kind);
       return wl.push(a, JSynthapp(openTForall(type, a), term, result, cont));
     }
     if (isTFun(type)) {
@@ -347,8 +412,8 @@ const step = wl => {
     if (type.tag === 'TMeta') {
       const i = indexTMeta(wl, type);
       if (i < 0) return terr(`undefined tmeta ${showType(type)}`);
-      const a = freshTMeta();
-      const b = freshTMeta();
+      const a = freshTMeta(kType);
+      const b = freshTMeta(kType);
       const ty = TFun(a, b);
       type.type = ty;
       replace2(wl, i, a, b);
@@ -361,6 +426,9 @@ const step = wl => {
     const { type1: type1_, type2, result, cont } = top;
     const type1 = pruneTop(type1_);
     if (type1.tag === 'TForall') {
+      const k = wfType(wl, type2);
+      if (!k) return terr(`type not wellformed: ${showJudgment(top)}`);
+      if (!eqKind(k, type1.kind)) return terr(`kind mismatch ${showJudgment(top)}`);
       result.type = openTForall(type1, type2);
       return wl.push(cont);
     }
@@ -377,20 +445,11 @@ const steps = wl => {
   }
 };
 
-const generalize = tm => {
-  const tvs = tmetas(tm).map(m => {
-    const x = `t${m.id}`;
-    m.type = TVar(x);
-    return x;
-  });
-  return tforall(tvs, prune(tm));
-};
-
 const infer = (term, ctx = []) => {
-  const tm = freshTMeta();
+  const tm = freshTMeta(kType);
   ctx.push(JSynth(term, tm, JDone));
   steps(ctx);
-  return generalize(tm);
+  return prune(tm);
 };
 
 // testing
@@ -403,18 +462,17 @@ const abs = (ns, b) => ns.reduceRight((x, y) => Abs(y, x), b);
 const tList = TVar('List');
 const tBool = TVar('Bool');
 
-const tid = TForall('t', TFun(tv('t'), tv('t')));
-const id = App(AppT(AbsT('t', Abs('x', Var('x')), TFun(tv('t'), tv('t'))), tid), Abs('y', Var('y')));
+const tid = TForall('t', kType, TFun(tv('t'), tv('t')));
 
 const ctx = initialContext();
 ctx.push(
-  tList,
-  tBool,
-  CVar('single', tforall(['t'], tfun(tv('t'), tapp(tList, tv('t'))))),
+  CTVar(tList.name, KFun(kType, kType)),
+  CTVar(tBool.name, kType),
+  CVar('single', tforall([['t', kType]], tfun(tv('t'), tapp(tList, tv('t'))))),
   CVar('True', tBool),
 );
 
-const term = abs(['f'], app(v('f'), app(v('single'), v('True'))));
+const term = AppT(Ann(abs(['x'], v('x')), tid), tBool);
 console.log(showTerm(term));
 const type = infer(term, ctx);
 console.log(showType(type));
